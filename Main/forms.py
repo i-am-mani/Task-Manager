fc353336afa8c1140703ab31c2d4836081553daf
Mandeep <mandeepch868@gmail.com>
2019-03-13 09:02:58 +0530

    Removed EditTaskForm and Added TeamCreationForm

--- Main/forms.py
+++ Main/forms.py
@@ -4,32 +4,26 @@ from .models import Team,UserTasks
 
 
 class TaskCreatationForm(forms.ModelForm):
-
     def __init__(self,*args,**kwargs):
         super(TaskCreatationForm,self).__init__(*args,**kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
-
         self.fields['task_title'].widget.attrs.update({"class":"form-control","placeholder":"Enter Title for task"})
         self.fields['task_description'].widget = forms.Textarea(attrs={"class":"form-control ","placeholder":"Enter Description for the task"})
         self.fields['task_comments'].widget = forms.Textarea(attrs={"class":"form-control ","placeholder":"Comments for task","rows":"5"})
         self.fields['task_comments'].required = False
-        
     class Meta:
-
         model = UserTasks
         fields =['task_title','task_description','task_comments']
 
-class EditTasksForm(forms.Form):
-
-    def __init__(self,user,*args,**kwargs):
-        self.field['task_to_edit']
-    
-
-    task_to_edit = forms.CharField(max_length=255,widget = forms.TextInput(attrs={'class':'form-control',"placeholder":"Enter New title"}))
-
-    new_title = forms.CharField(max_length=255,widget = forms.TextInput(attrs={'class':'form-control',"placeholder":"Enter New title"}))
-    new_description = forms.CharField(max_length=4000,widget = forms.Textarea(attrs={'class':'form-control','placeholder':'Enter New Description'}))
-
-
+class TeamCreatationForm(forms.ModelForm):
+    def __init__(self,*args,**kwargs):
+        super(TeamCreatationForm,self).__init__(*args,**kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+        self.fields['name'].widget.attrs.update({"class":"form-control","placeholder":"Enter Name for Team"})
+        self.fields['description'].widget.attrs.update({"placeholder":"Enter A Short Description About Your Team"})
+    class Meta:
+        model = Team
+        fields = ['name','description']
 
