29e0e22232930f50a496107a4aee8dcdd6c9175a
Mandeep <mandeepch868@gmail.com>
2019-03-24 20:57:08 +0530

    Refractored

--- Main/models.py
+++ Main/models.py
@@ -4,20 +4,19 @@ from django.conf import settings
 
 
 class Team(models.Model):
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='creator')
-    name = models.CharField(max_length=200,unique=True)
-    description = models.CharField(max_length=3000,null=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='creator')
+    name = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=3000, null=True)
     members = models.ManyToManyField(settings.AUTH_USER_MODEL)
     date_of_creation = models.DateField(auto_now=True)
 
 
 class UserTasks(models.Model):
-    PLANNED='planned'
+    PLANNED ='planned'
     INPROGRESS = 'inprogress'
     DONE = 'done'
-    _choices = [(PLANNED,'planned'),(INPROGRESS,'inprogress'),(DONE,'done')]
+    _choices = [(PLANNED, 'planned'),(INPROGRESS, 'inprogress'),(DONE, 'done')]
 
-    #fields
     task_assignee =  models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)  #https://docs.djangoproject.com/en/2.1/topics/auth/customizing/#referencing-the-user-model
     task_assigner =  models.ForeignKey(Team,on_delete=models.CASCADE,null=True)
     task_title = models.CharField(max_length=255,unique=True) 
