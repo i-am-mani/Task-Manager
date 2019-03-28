d53dbda986d67d9c3dd5ba7421d0d168ca30cf52
Mandeep <mandeepch868@gmail.com>
2019-03-28 19:26:26 +0530

    Added Task Comment model Modified task_assigner field to point to User creating task Added extra field in UserTask, Team.To point to Team to which task belongs.

--- Main/models.py
+++ Main/models.py
@@ -10,29 +10,24 @@ class Team(models.Model):
     members = models.ManyToManyField(settings.AUTH_USER_MODEL)
     date_of_creation = models.DateField(auto_now=True)
 
+class TaskComment(models.Model):
+    date_of_creation = models.DateField(auto_now=True)
+    comment_content = models.CharField(max_length=3000)
 
 class UserTasks(models.Model):
     PLANNED ='planned'
     INPROGRESS = 'inprogress'
     DONE = 'done'
     _choices = [(PLANNED, 'planned'),(INPROGRESS, 'inprogress'),(DONE, 'done')]
-
-    task_assignee =  models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)  #https://docs.djangoproject.com/en/2.1/topics/auth/customizing/#referencing-the-user-model
-    task_assigner =  models.ForeignKey(Team,on_delete=models.CASCADE,null=True)
+    # #https://docs.djangoproject.com/en/2.1/topics/auth/customizing/#referencing-the-user-model
+    task_assignee =  models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="assignee")
+    task_assigner =  models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name="assigner")
+    task_team = models.ForeignKey(Team,on_delete=models.CASCADE,null=True,related_name="belongs_to_team")
     task_title = models.CharField(max_length=255,unique=True) 
     task_description = models.CharField(max_length=4000)
     task_status = models.CharField(choices = _choices,default=PLANNED,max_length=255)
     date_of_creation = models.DateField(auto_now=True)
-    task_comments = models.CharField(max_length=3000,null=True)
-
-
-
-
-
-
-
-
-
+    task_comments = models.ManyToManyField(TaskComment,related_name="comments")
 
 
 
